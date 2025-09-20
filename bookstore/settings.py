f505309d400891ce93774c5fc51a5b8a5cbe7f59
LindsellR <lindsellrichard@gmail.com>
2025-09-20 12:07:38 +1000

    Debug back to False

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # Make CSRF and session cookies work over HTTPS
 CSRF_COOKIE_SECURE = True
